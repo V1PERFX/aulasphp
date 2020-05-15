2380f7e3ecb2a9d7602f8207c890add9902c6159
V1PERFX <rafael_fiuza@ymail.com>
2020-05-15 17:58:38 -0300

    realizado a programação

--- aula5at2-cliente.php
+++ aula5at2-cliente.php
@@ -1,3 +1,65 @@
 <?php
+abstract class Cliente {
+    private string $nome;
+    private string $endereco;
+    private string $cidade;
+    private string $uf;
+    private int $cep;
 
+    public function __construct($nome, $endereco, $cidade, $uf, $cep){
+        $this->nome = $nome;
+        $this->endereco = $endereco;
+        $this->cidade = $cidade;
+        $this->uf = $uf;
+        $this->cep = $cep;
+    }
+
+    public function IcluirNovoCliente() {
+        //programar
+    }
+
+    public function AtualizarCliente() {
+        //programar
+    }
+}
+
+class PessoaFisica extends Cliente {
+    private int $cpf;
+    private int $rg;
+    private string $sexo;
+    private string $dataNascimento;
+
+    public function __construct($nome, $endereco, $cidade, $uf, $cep, $cpf,
+        $rg, $sexo, $dataNascimento){
+        parent::__construct($nome, $endereco, $cidade, $uf, $cep);
+        $this->cpf = $cpf;
+        $this->rg = $rg;
+        $this->sexo = $sexo;
+        $this->dataNascimento = $dataNascimento;
+    }
+
+    public function ValidarCPF() {
+        //programar
+    }
+    
+}
+
+class PessoaJuridica extends Cliente {
+    private int $cnpj;
+    private string $inscricaoEstadual;
+    private string $razaoSocial;
+
+    public function __construct($nome, $endereco, $cidade, $uf, $cep, $cnpj,
+    $inscricaoEstadual, $razaoSocial){
+    parent::__construct($nome, $endereco, $cidade, $uf, $cep);
+    $this->cnpj = $cnpj;
+    $this->inscricaoEstadual = $inscricaoEstadual;
+    $this->razaoSocial = $razaoSocial;
+}
+
+    public function ValidarCNPJ() {
+        //programar
+    }
+
+}
 ?>
